fed64c0ecf1a428c24516b56fdc2d8fd52547081
delano campos <delanoacampos@gmail.com>
2026-04-29 17:58:46 -0500

    H-> gg style summary plotter for all lifetimes, masses

---
+++ run_summary.py
@@ -0,0 +1,12 @@
+import ROOT
+import plot_summary
+
+def main():
+    for mass in ['15','20','30','40','50','55']:
+        for lifetime in ["0", "10", "20", "50", "100", "1000"]:
+            for year in ["2016", "2017", "2018","2022", "2023","2024"]:
+                plot_summary.run(mass, lifetime, year)
+
+if __name__ == "__main__":
+    main()
+
